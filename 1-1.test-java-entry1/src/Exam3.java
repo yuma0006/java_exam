ce2bdf28a55311d6ffe5003faf1373b685c71596
y.kosaka <yuma.kosaka@rakus-partners.co.jp>
2026-01-19 10:15:31 +0900

    testの解答を修正

--- 1-1.test-java-entry1/src/Exam3.java
+++ 1-1.test-java-entry1/src/Exam3.java
@@ -4,8 +4,8 @@ public class Exam3 {
         int price2 = 250;
         double tax = 0.1;
         int subTotal = (price1 * 3 + price2 * 4);
-        int totalTax = ((int)(price1 * 3 + price2 * 4 * tax));
-        int total = ((subTotal + (int)totalTax));
+        int totalTax = ((int)( subTotal * tax));
+        int total = (((int)subTotal + totalTax));
 
         System.out.println("小計\n" + subTotal + "円");
         System.out.println("消費税\n" + totalTax + "円");
--- 1-1.test-java-entry1/src/Exam6.java
+++ 1-1.test-java-entry1/src/Exam6.java
@@ -1,6 +1,6 @@
 public class Exam6 {
     public static void main(String[] args) {
-        String statement = 
+        String statement = "Steve Jobs said\n\"Stay Hungry. Stay Foolish.\"";
         System.out.println(statement);
     }
     
